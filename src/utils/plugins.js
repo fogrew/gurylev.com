6d92916ccb46fe3e11f5eb18991ce5a54b5b9b7c
Andrew Gurylev <andrew@gurylev.com>
2021-03-28 18:41:42 +0300

    feat: add pwa
    resolves #9

--- src/utils/plugins.js
+++ src/utils/plugins.js
@@ -2,6 +2,7 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const pluginTOC = require("eleventy-plugin-nesting-toc");
+const pluginPWA = require("eleventy-plugin-pwa");
 
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
@@ -12,6 +13,18 @@ module.exports = {
     eleventyConfig.addPlugin(pluginRss);
     eleventyConfig.addPlugin(pluginSyntaxHighlight);
     eleventyConfig.addPlugin(pluginNavigation);
+    eleventyConfig.addPlugin(pluginPWA, {
+      cacheId: 'gurylev',
+      globPatterns: [
+        "**/*.{html,css,js,mjs,map,jpg,png,gif,webp,avif,ico,svg,woff2,woff,eot,ttf,otf,ttc,json}"
+      ],
+      runtimeCaching: [
+        {
+          urlPattern: /^.*\.(html|css|jpg|png|gif|webp|ico|svg|woff2|woff|eot|ttf|otf|ttc|json)$/,
+          handler: `staleWhileRevalidate`
+        }
+      ]
+    });
     eleventyConfig.addPlugin(pluginTOC, {
       headingText: '',
       wrapperClass: 'toc__nav'
