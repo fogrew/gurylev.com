68f1b17cc483909e116d0da63badd95705b53b22
Andrew Gurylev <andrew@gurylev.com>
2021-04-29 00:10:29 +0300

    feat: add reading time to articles; resolve #39

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -6,12 +6,43 @@ module.exports = {
   published: (posts) => {
     return posts.filter(post => !post.data.draft)
   },
+
+  readingTime: (postOrContent) => {
+    if (!postOrContent || typeof postOrContent !== 'string') {
+      return ``;
+    }
+
+    const content = htmlContent.replace(/(<([^>]+)>)/gi, '');
+    const matches = content.match(/[\u0400-\u04FF]+|\S+\s*/g);
+    const count = matches !== null ? matches.length : 0;
+    const speed = 300;
+    const estimatedTime = Math.ceil(count / speed);
+
+    return estimatedTime;
+  },
+
   readableDate: (dateObj) => {
     return new Intl.DateTimeFormat('ru', {
       year: 'numeric',
       month: 'short',
       day: 'numeric'
     }).format(new Date(dateObj))
+  },
+
+  getNoun: (number, one, two, five) => {
+    let n = Math.abs(number);
+    n %= 100;
+    if (n >= 5 && n <= 20) {
+      return five;
+    }
+    n %= 10;
+    if (n === 1) {
+      return one;
+    }
+    if (n >= 2 && n <= 4) {
+      return two;
+    }
+    return five;
   },
 
   debugger: (...args) => {
