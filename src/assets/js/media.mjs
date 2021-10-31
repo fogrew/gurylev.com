de1c63712ac3a902184ee2be312d34290e0c6532
Andrew Gurylev <andrew@gurylev.com>
2021-11-01 02:11:07 +0300

    feat: add seekable to table of contents for media

--- src/assets/js/media.mjs
+++ src/assets/js/media.mjs
@@ -1,5 +1,6 @@
-const mediaElement = document.querySelector('video, audio');
+const mediaElement = document.querySelector('video, audio')
 const timePrefix = '#t='
+let seeking = 0
 
 const goTime = () => {
   const { hash } = location
@@ -8,12 +9,35 @@ const goTime = () => {
   mediaElement.currentTime = hash.slice(timePrefix.length)
 }
 
+const getShortTime = () => {
+  return parseInt(mediaElement.currentTime)
+}
+
 const saveEndedState = () => {
-  const shortTime = parseInt(mediaElement.currentTime)
-  history.replaceState(undefined, undefined, `${timePrefix}${shortTime}`)
+  history.replaceState(undefined, undefined, `${timePrefix}${getShortTime()}`)
+}
+
+const getSubtitles = () => {
+}
+
+const updateTableOfContents = () => {
+  const activeCueTime = mediaElement?.textTracks[0]?.activeCues[0]?.startTime
+
+  if(!activeCueTime) return
+  if(activeCueTime == seeking) return
+
+  const activeTimeLink = document.querySelector(`.seekable[href="#t=${activeCueTime}"]`)
+  const prevTimeLink = document.querySelector(`.seekable.seeking`)
+
+  activeTimeLink?.classList?.add('seeking')
+  prevTimeLink?.classList?.replace('seeking', 'seeked')
+
+  seeking = activeCueTime
 }
 
 mediaElement.addEventListener('pause', saveEndedState)
+// mediaElement.addEventListener('timeupdate', updateTableOfContents)
+mediaElement.addEventListener('timeupdate', updateTableOfContents)
 // mediaElement.addEventListener('timeupdate', saveEndedState)
 // stop-stop-stop, cowboy! take care of the performance please
 // TODO: add debounce
