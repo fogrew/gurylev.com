5c76026cdde49742f55c7848c5fbdc09b858c398
Andrew Gurylev <andrew@gurylev.com>
2021-11-07 09:27:00 +0300

    build: make dev env, when env not set (for local)

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  env: process.env.ELEVENTY_ENV,
+  env: process.env.ELEVENTY_ENV || 'development',
   release: (new Date()).toLocaleString(),
   age: new Date().getFullYear() - new Date(1993, 0, 7).getFullYear()
 };
