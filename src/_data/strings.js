0fdc250babcce41e2c1535a068f2582309aaab17
Andrew Gurylev <andrew@gurylev.com>
2021-12-30 12:34:35 +0300

    feat(i18n): translate base layout to en

---
+++ src/_data/strings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  en: {
+    "Skip navigation": "Skip navigation",
+    "Drafts": "Drafts",
+    "Andrey Gurylev's website": "Andrey Gurylev's website",
+    "about site": "about site",
+    "Edit this page": "Edit this page"
+  },
+  ru: {
+    "Skip navigation": "Пропустить навигацию",
+    "Drafts": "Черновики",
+    "Andrey Gurylev's website": "Сайт Андрея Гурылева",
+    "about site": "о сайте",
+    "Edit this page": "Редактировать эту страницу"
+  },
+}
