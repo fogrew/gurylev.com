a3fd5bf2cfb44433e00828a527921cbaed045709
Andrew Gurylev <andrew@gurylev.com>
2022-03-21 22:14:28 +0200

    build: disable card shortcut

--- src/utils/shortcodes.js
+++ src/utils/shortcodes.js
@@ -78,26 +78,25 @@ module.exports = {
         ${itemprop ? 'itemprop='+itemprop : ''}>
     </picture>`;
   },
-  card: function({ url, title, date, text, imageLink, tag, classes } = {
-    url,
-    title,
-    date,
-    text,
-    imageLink,
-    tag: 'article',
-    classes: ''
-  }) {
-    this.getFilter('lol')
-    const image = imageLink ? `<span class="card__preview">${this.image(imageLink, 'card__image', 'photo of my cat')}</span>` : '';
-    return `<${ tag } class="${ classes } card">
-    <a class="card__link" href="${ url }">
-      <span class="card__body">
-        <h3 class="card__title">${ title }</h3>
-        <time class="card__date" datetime="${ this.htmlDateString(date) }">${ this.readableDate(date) }</time>
-        <p>${ text }</p>
-      </span>
-      ${image}
-    </a>
-  </${ tag }>`;
-  }
+  // card: function({ url, title, date, text, imageLink, tag, classes } = {
+  //   url,
+  //   title,
+  //   date,
+  //   text,
+  //   imageLink,
+  //   tag: 'article',
+  //   classes: ''
+  // }) {
+  //   const image = imageLink ? `<span class="card__preview">${this.image(imageLink, 'card__image', 'photo of my cat')}</span>` : '';
+  //   return `<${ tag } class="${ classes } card">
+  //   <a class="card__link" href="${ url }">
+  //     <span class="card__body">
+  //       <h3 class="card__title">${ title }</h3>
+  //       <time class="card__date" datetime="${ this.htmlDateString(date) }">${ this.readableDate(date) }</time>
+  //       <p>${ text }</p>
+  //     </span>
+  //     ${image}
+  //   </a>
+  // </${ tag }>`;
+  // }
 };
