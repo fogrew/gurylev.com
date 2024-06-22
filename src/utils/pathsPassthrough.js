9031f0c0825a16be0bf4bfec17f14f1dc546c285
Andrew Gurylev <andrew@gurylev.com>
2024-06-22 17:02:46 +0300

    feat: add keybase proof

--- src/utils/pathsPassthrough.js
+++ src/utils/pathsPassthrough.js
@@ -4,6 +4,7 @@ module.exports = [
   { "src/assets/keys": "keys" },
   { "src/assets/media": "media" },
   { "src/assets/fonts": "fonts" },
+  { "src/assets/.well-known": ".well-known" },
   { "src/assets/favicon.png": "favicon.png" },
   { "src/assets/robots.txt": "robots.txt" },
   { "src/assets/budget.json": "budget.json" },
